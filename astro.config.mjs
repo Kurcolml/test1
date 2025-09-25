ecffc6447b60e4a909512502f38c208eaaec9d75
Manuel Blanco <manuel.blanco.1995@gmail.com>
2025-09-24 21:20:06 -0300

    subiendo a github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/test/', // Cambia aquí al nombre de tu repo
+  output: 'static',
+});
