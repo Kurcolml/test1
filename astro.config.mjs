0194a97f4abe5ca73e7edb5ea40807bef02bb74e
Manuel Blanco <manuel.blanco.1995@gmail.com>
2025-09-24 22:12:53 -0300

    llamando imagenes desde el public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  base: '/test1/',  // 🔥 Nombre exacto del repositorio
-  output: 'static'
+  base: "/test1/", // 👈 el mismo nombre del repositorio en GitHub
 });
